f1d7da0faef8888ca64ae2a6975c81988769c842
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-05 21:05:04 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,5 +1,9 @@
 function solution(strlist) {
+    /* ---- 기존 코드 ----
     let answer = [];
     strlist.forEach((str) => answer.push(str.length));
     return answer;
+    */
+    
+    return strlist.map((str) => str.length);
 }

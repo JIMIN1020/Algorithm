eb76990c739ea4404a25306547fae8e8685b6a57
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-05 20:49:25 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,10 +1,3 @@
 function solution(my_string) {
-  const vowel = ["a", "e", "i", "o", "u"];  // 모음
-    
-  // 하나씩 확인 후 제거
-  vowel.forEach((v) => {
-    const regex = new RegExp(`${v}`, "g");  // 변수로 정규표현식 만들기
-    my_string = my_string.replace(regex, "");
-  });
-  return my_string;
+  return my_string.replace(/[aeiou]/g, "");
 }

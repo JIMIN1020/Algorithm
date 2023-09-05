e48460809f8b1afbaae3fcf6b19591390cd13f8f
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-05 20:10:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,11 +1,7 @@
 function solution(array) {
     const count = Array.from({length: 1000}, () => 0); // 배열 생성 및 초기화
-    array.forEach((num) => count[num]++);   // 카운트하기
+    array.forEach((num) => count[num]++);   // 카운트 하기
     
-    const maxCount = Math.max(...count);
-    let maxValue = [];
-    for (let i = 0; i < 1000; i++) {
-        count[i] === maxCount ? maxValue.push(i) : undefined;
-    }
-    return maxValue.length === 1 ? maxValue[0] : -1;
+    const max = Math.max(...count); // 최댓값 구하기
+    return count.indexOf(max) === count.lastIndexOf(max) ? count.indexOf(max) : -1;
 }

913216e7e46df36f90ac90e9350304596345150e
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-12 22:25:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
     let answer = 0;
-    my_string.split("").filter((s) => !isNaN(s)).map(Number).forEach((n) => answer+=n);
+    [...my_string].filter((s) => !isNaN(s)).map(Number).forEach((n) => answer+=n);
     return answer;
 }

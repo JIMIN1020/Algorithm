9fdb1f5e6791127912d3a40a6971b0ac88549ff7
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-20 19:57:52 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,6 +1,4 @@
 function solution(num) {
-    num.sort((a, b) => b-a);
-    let max = num[0] * num[1];
-    let min = num[num.length-1] * num[num.length-2];
-    return Math.max(max, min);
+    num.sort((a, b) => a-b);
+    return Math.max(num[0] * num[1], num[num.length-1] * num[num.length-2]);
 }

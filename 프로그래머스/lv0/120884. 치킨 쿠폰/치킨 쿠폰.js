080e53a3bb80870e8e5c8b0838da0b71ba685bc6
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-24 20:12:00 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,8 +1,9 @@
 function solution(chicken) {
-    const coupon = (order) => {
-        if (order < 10) return 0;
-        return Math.floor(order / 10) + coupon(Math.floor(order / 10) + order % 10);
-    }
     return coupon(chicken)
 }
 
+// 재귀 함수 (10으로 나누기 -> 서비스 치킨 + 나머지 값으로 또 다시 호출
+const coupon = (order) => {
+    if (order < 10) return 0;
+    return Math.floor(order / 10) + coupon(Math.floor(order / 10) + order % 10);
+}

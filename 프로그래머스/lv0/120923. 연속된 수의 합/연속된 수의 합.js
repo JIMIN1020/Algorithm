3dd4727004e636f223eab3b94d4ec87125c14b37
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-09-25 19:52:42 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -4,6 +4,7 @@ function solution(num, total) {
     let mid = Math.round(total / num);
     ans.push(mid);
     
+    // 앞 뒤로 연속된 수 담기
     let i = 1;
     while (ans.length < num) {
         ans.push(mid+i);
@@ -11,6 +12,7 @@ function solution(num, total) {
         i++;
     }
     
+    // num보다 더 많은 경우 초과값 제거
     if (ans.length > num) {
         let n = total - ans.reduce((a, c) => a + c);
         ans.splice(ans.indexOf(n), 1);

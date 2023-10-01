41a6e58b096b2b1467056163c18516ac375019c4
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-10-01 17:52:32 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.58 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,11 +1,9 @@
 function solution(arr, queries) {
     let answer = [];
-    
     queries.forEach(([s, e, k]) => {
-        let list = [...arr].slice(s, e+1);
-        list = list.filter((v) => v > k);
-        answer.push(list.length ? Math.min(...list) : -1);
+        let list = [...arr].slice(s, e+1);  // s ~ e 범위 잘라내기
+        list = list.filter((v) => v > k);   // k보다 큰 값 필터링
+        answer.push(list.length ? Math.min(...list) : -1);  // 최솟값 or -1 push
     })
-    
     return answer;
 }

9482c1a147703b2704bd2091c9bc423ba1d3ddbd
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-10-12 13:25:33 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
-    const day = ["THU","FRI","SAT","SUN","MON","TUE","WED"];
+    const day = ["FRI","SAT","SUN","MON","TUE","WED","THU"];
     const numOfDay = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let totalDays = numOfDay.slice(0, a).reduce((a, c) => a + c) + b;
+    let totalDays = numOfDay.slice(0, a).reduce((a, c) => a + c) + b - 1;
     
     return day[totalDays % 7];
 }

3547cd84257ebc70b02e123874df54a47dcb3885
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-10-16 17:05:28 +0900

    [level 1] Title: 기사단원의 무기, Time: 3.26 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -4,9 +4,12 @@ function solution(number, limit, power) {
     // 약수 개수 구하기
     for (let i = 1; i <= number; i++) {
         let divisors = [];
+        
+        // 제곱근까지만 약수 구하기
         for (let j = 1; j <= Math.sqrt(i); j++) {
             if(i % j === 0) {
 		        divisors.push(i);
+                // 구한 약수로 나눈 수 = 또 다른 약수
 		        if(i / j != j) divisors.push(i / j);
             }
         }

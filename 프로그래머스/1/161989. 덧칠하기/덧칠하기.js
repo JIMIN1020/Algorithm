85d605acc3e1571a694cd94a33f927c9eb32741e
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2023-10-16 17:40:01 +0900

    [level 1] Title: 덧칠하기, Time: 2.43 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,12 @@
-function solution(n, m, sec) {
-    let count = 0;
+function solution(n, m, section) {
+    // 색칠할 벽
     let wall = Array(n+1).fill(0);
     
-    // 색칠할 부분
-    sec.forEach((v) => wall[v] = 1);
+    // 색칠할 부분 1로 체크
+    section.forEach((v) => wall[v] = 1);
     
-    // 
+    // 1이 나오면 카운트하고, m만큼 더해서 다음 부분으로 넘어가기
+    let count = 0;
     for (let i = 1; i <= n; ) {
         if (wall[i] === 1) {
             i += m;

2f3ae341846b6c86358959b2552457f1a8864d3c
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2024-01-08 15:50:29 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 3.23 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,7 +1,5 @@
 function solution(arr, queries) {
-    queries.forEach((q) => {
-        const [s, e, k] = q;
-        
+    queries.forEach(([s, e, k]) => {
         for (let i = s; i <= e; i++) {
             if (i % k === 0) arr[i] += 1;
         }

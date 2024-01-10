caadcd0267731a7a65e05b02c3dc631104c3d762
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2024-01-10 16:58:17 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,24 @@
+function solution(new_id) {
+  let id = new_id;
+
+  // 1~4단계
+  id = id
+    .toLowerCase()
+    .replaceAll(/[^a-z0-9\-_.]/g, "")
+    .replaceAll(/\.+/g, ".")
+    .replaceAll(/^\.+|\.+$/g, "");
+
+  // 5단계
+  if (id === "") id = "a";
+
+  // 6단계
+  if (id.length >= 16) id = id.slice(0, 15);
+  if (id[id.length - 1] === ".") id = id.slice(0, 14);
+
+  // 7단계
+  while (id.length < 3) {
+    id += id[id.length - 1];
+  }
+
+  return id;
+}

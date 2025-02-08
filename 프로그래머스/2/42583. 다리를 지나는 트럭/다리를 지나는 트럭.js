5c63422de2489618600adf9be301b73491cb28ca
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2025-02-08 19:48:09 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.90 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -15,6 +15,9 @@ function solution(bridge_length, weight, truck_weights) {
         if (current.reduce((a, c) => a + c[0], 0) + truck_weights[0] <= weight) {
             const truck = truck_weights.shift();
             current.push([truck, totalTime + bridge_length]);
+        } else if (current[0]) {
+            // 못올라오는 경우 시간 점프
+            totalTime = current[0][1] - 1;
         }
     }
     

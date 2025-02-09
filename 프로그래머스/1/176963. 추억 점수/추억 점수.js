87802fae1395890403987d4252acbf12191c9865
Jimin <121474189+JIMIN1020@users.noreply.github.com>
2025-02-09 18:53:56 +0900

    [level 1] Title: 추억 점수, Time: 1.13 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,9 +1,7 @@
 function solution(name, yearning, photo) {
-    // name에 해당하는 yearning으로 바꿔주기 (없을 경우 0)
-    let cal = photo.map((v) => {
-        return v.map((e) => name.indexOf(e) === -1 ? 0 : yearning[name.indexOf(e)]);
-    });
+    const point = {};
     
-    // 바꾼 값 합친 후 제출
-    return cal.map((v) => v.reduce((a, c) => a + c, 0));
+    name.forEach((n, i) => point[n] = yearning[i]);
+    
+    return photo.map((p) => p.reduce((a, c) => a + (point[c] || 0), 0));
 }
